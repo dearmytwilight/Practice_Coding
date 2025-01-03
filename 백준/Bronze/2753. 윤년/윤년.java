954bd1466277961acebe7d9fdc37b41f52ec227c
됸깅잉 <155629004+dearmytwilight@users.noreply.github.com>
2025-01-04 02:40:37 +0900

    [Bronze V] Title: 윤년, Time: 168 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+
+        int b = a%100;
+
+        if (b%4 == 0) {
+            if (a%100 != 0 || a%400==0) {
+                System.out.println("1");
+            }
+
+            else {
+                System.out.println("0");
+            }
+        }
+
+        else {
+            System.out.println("0");
+        }
+    }
+}
