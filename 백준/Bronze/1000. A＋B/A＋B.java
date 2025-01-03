ccaf72a640725a2a42a29341e6b7bc83ae1d288e
됸깅잉 <155629004+dearmytwilight@users.noreply.github.com>
2025-01-04 00:49:58 +0900

    [Bronze V] Title: A+B, Time: 172 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // 정수 입력
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+
+        // 출력
+        System.out.println(a+b);
+
+        scanner.close(); // 리소스 해제
+
+    }
+}
