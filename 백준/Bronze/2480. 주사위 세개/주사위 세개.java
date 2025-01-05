aa16bfb6d01ecda01aa1f90f7633f51350c87db4
됸깅잉 <155629004+dearmytwilight@users.noreply.github.com>
2025-01-05 16:46:35 +0900

    [Bronze IV] Title: 주사위 세개, Time: 172 ms, Memory: 17648 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -10,45 +10,18 @@ public class Main {
         int money = 0;
 
 
-        if (a == b) {
-            if (b == c) {
-                money = 10000 + a*1000;
-            }
-
-            else {
-                money = 1000 + a*100;
-            }
-        }
-
-        else if (b == c) {
-            money = 1000 + b*100;
-        }
-
-        else if (a == c) {
-            money = 1000 + a*100;
-        }
-
-        else {
-            if (a > b) {
-                if (a > c) {
-                    money = a*100;
-                }
-
-                else {
-                    money = c*100;
-                }
-            }
-
-            else {
-                if (b > c) {
-                    money = b*100;
-                }
-
-                else {
-                    money = c*100;
-                }
-            }
-        }
+       if (a==b && b==c) {
+           money = 10000+a*1000;
+       }
+       else if (a==b || a==c) {
+           money = 1000 + a*100;
+       }
+       else if (b == c) {
+           money = 1000 + b*100;
+       }
+       else {
+           money = Math.max(Math.max(a,b),c)*100;
+       }
 
         System.out.println(money);
 
