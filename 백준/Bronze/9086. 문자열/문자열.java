00e686ecb6f9e01638588844ba2ec22152dc1615
됸깅잉 <155629004+dearmytwilight@users.noreply.github.com>
2025-01-07 18:28:58 +0900

    [Bronze V] Title: 문자열, Time: 176 ms, Memory: 17164 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for (int i=1; i<=n; i++) {
+            String str = scanner.next();
+            System.out.print(str.charAt(0));
+            System.out.println(str.charAt(str.length()-1));
+
+        }
+    }
+
+}
